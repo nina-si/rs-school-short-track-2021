892937012380a83e61afe577082e96ed22745d8b
Nina Sitaeva <sitaeva.nn@gmail.com>
2021-04-11 22:45:16 +0300

    feat: implement function 05-delete-digits

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,20 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const digits = n.toString(10).split('');
+  const result = [];
+  let additional = digits.slice(0);
+
+  for (let i = 0; i < digits.length; i++) {
+    additional.splice(i, 1);
+    result.push(Number(additional.join('')));
+    additional = digits.slice(0);
+  }
+
+  result.sort((a, b) => a - b);
+
+  return result[result.length - 1];
 }
 
 module.exports = deleteDigit;
