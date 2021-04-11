2c92dd4072dec34c0e2245379496fd7fc2687e88
Nina Sitaeva <sitaeva.nn@gmail.com>
2021-04-11 20:48:09 +0300

    feat: implement task 04-mine-sweeper

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,72 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  let count = 0;
+  const result = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    result.push(matrix.slice(0));
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (i === 0) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (j === 0) {
+          count = matrix[i][j + 1] + matrix[i + 1][j] + matrix[i + 1][j + 1];
+          result[i][j] = count;
+          count = 0;
+        } else if (j === matrix[i].length - 1) {
+          count = matrix[i][j - 1] + matrix[i + 1][j] + matrix[i + 1][j - 1];
+          result[i][j] = count;
+          count = 0;
+        } else {
+          count = matrix[i][j - 1] + matrix[i][j + 1] + matrix[i + 1][j]
+              + matrix[i + 1][j - 1] + matrix[i + 1][j + 1];
+          result[i][j] = count;
+          count = 0;
+        }
+      }
+    } else if (i === matrix.length - 1) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (j === 0) {
+          count = matrix[i][j + 1] + matrix[i - 1][j] + matrix[i - 1][j + 1];
+          result[i][j] = count;
+          count = 0;
+        } else if (j === matrix[i].length - 1) {
+          count = matrix[i][j - 1] + matrix[i - 1][j] + matrix[i - 1][j - 1];
+          result[i][j] = count;
+          count = 0;
+        } else {
+          count = matrix[i][j - 1] + matrix[i][j + 1] + matrix[i - 1][j]
+               + matrix[i - 1][j - 1] + matrix[i - 1][j + 1];
+          result[i][j] = count;
+          count = 0;
+        }
+      }
+    } else {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (j === 0) {
+          count = matrix[i][j + 1] + matrix[i - 1][j] + matrix[i - 1][j + 1]
+                + matrix[i + 1][j] + matrix[i + 1][j + 1];
+          result[i][j] = count;
+          count = 0;
+        } else if (j === matrix[i].length - 1) {
+          count = matrix[i][j - 1] + matrix[i - 1][j] + matrix[i - 1][j - 1]
+                + matrix[i + 1][j] + matrix[i + 1][j - 1];
+          result[i][j] = count;
+          count = 0;
+        } else {
+          count = matrix[i][j - 1] + matrix[i][j + 1] + matrix[i - 1][j]
+              + matrix[i - 1][j - 1] + matrix[i - 1][j + 1]
+              + matrix[i + 1][j] + matrix[i + 1][j - 1] + matrix[i + 1][j + 1];
+          result[i][j] = count;
+          count = 0;
+        }
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = minesweeper;
